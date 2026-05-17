c381fd8696f179612cfef301792dd4f2eb78acdc
putriayupm <putriayupm06@gmail.com>
2026-05-17 21:45:23 +0700

    finishing

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://filmroll-frontend.vercel.app',
+    'https://filmroll-frontend-8bpz5ooik-putriayupms-projects.vercel.app'
+  ]
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
@@ -13,6 +19,7 @@ app.use('/api/recommendations', require('./routes/recommendations'));
 app.use('/api/watchlist', require('./routes/watchlist'));
 app.use('/api/history', require('./routes/history'));
 app.use('/api/watchparty', require('./routes/watchparty'));
+app.use('/api/watch-party', require('./routes/watchparty'));
 
 app.get('/', (req, res) => res.send('Watch-List Roulette Backend Running!'));
 
